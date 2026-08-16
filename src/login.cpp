824afb4827e3793a34053ab878399d956130dbbe
varshithavallabhadasu-commits <varshithavallabhadasu@gmail.com>
2026-08-16 18:01:12 +0530

    Normalize usernames to uppercase for case-insensitive login

--- src/login.cpp
+++ src/login.cpp
@@ -1,14 +1,15 @@
 #include "login.h"
+#include "utils.h"
 
 void LoginManager::registerUser(const std::string& username, const std::string& password) {
     if (password.empty()) {
         return;
     }
 
-    users[username] = password;
+    users[toUpper(username)] = password;
 }
 
 bool LoginManager::authenticate(const std::string& username, const std::string& password) const {
-    auto it = users.find(username);
+    auto it = users.find(toUpper(username));
     return it != users.end() && it->second == password;
 }
